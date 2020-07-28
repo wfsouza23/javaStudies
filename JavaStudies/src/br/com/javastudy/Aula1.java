bcc2c4aaa301469df00ef0b6e3aca47995b0b436
Wesley Souza <wfsouza23@gmail.com>
2020-07-28 20:05:30 -0300

    New line for the first class

--- JavaStudies/src/br/com/javastudy/Aula1.java
+++ JavaStudies/src/br/com/javastudy/Aula1.java
@@ -1,5 +1,12 @@
 package br.com.javastudy;
 
 public class Aula1 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Git");
+		System.out.println("Hello Wes");
+		
+	}
 
 }
