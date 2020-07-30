d690df356cd64b30124b23cdb4c21ea8b62916aa
Wesley Souza <wfsouza23@gmail.com>
2020-07-29 21:28:44 -0300

    new class

---
+++ JavaStudies/src/br/com/javastudy/Aula2.java
@@ -0,0 +1,13 @@
+package br.com.javastudy;
+
+import java.util.Scanner;
+
+public class Aula2 {
+	public static void main(String[] args) {
+		try(Scanner myValue = new Scanner(System.in)){
+			System.out.println(myValue.nextLine());
+		}
+		
+	}
+	 
+}
