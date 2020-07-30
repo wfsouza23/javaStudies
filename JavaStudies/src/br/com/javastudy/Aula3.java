982c653801b5a604c6901b35d84c881d606ac064
Wesley Souza <wfsouza23@gmail.com>
2020-07-30 18:40:49 -0300

    class 3

---
+++ JavaStudies/src/br/com/javastudy/Aula3.java
@@ -0,0 +1,17 @@
+package br.com.javastudy;
+
+public class Aula3 {
+	
+	public static void main(String[] args) {
+		int result = 0;
+		for (int i = 0; i < 5; i++) {
+			if (i == 3) {
+				result += 10;
+			} else {
+				result += i;
+			}
+		}
+		System.out.println(result);
+	}
+
+}
