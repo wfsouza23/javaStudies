69376982e205fb0cbbfb2b35f15b9e51baf5b48e
Wesley Souza <wfsouza23@gmail.com>
2020-07-29 21:27:04 -0300

    new code

--- JavaStudies/src/br/com/javastudy/Aula1.java
+++ JavaStudies/src/br/com/javastudy/Aula1.java
@@ -5,7 +5,7 @@ public class Aula1 {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello Git");
-		System.out.println("Hello Wes");
+		System.out.println("Hello Wesley");
 		
 	}
 
